e0f2eea86beb5ab85627beca70d5cd2002cbe43f
mitre.cool <mitre.cool@gmail.com>
2019-07-05 22:11:37 +0700

    1. Added api versioning 2. Added new methods 3. Added swagger 3.0 generation

---
+++ MyWebApi/Controllers/ValuesV2Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApi.Models;
+
+namespace MyWebApi.Controllers
+{
+    [ApiVersion("2.0")]
+    [Produces("application/json")]
+    [Route("api/v{version:apiVersion}/values")]
+    [ApiController]
+    public class ValuesV2Controller : ControllerBase
+    {        
+        [HttpGet]
+        [Route("retrieveGoods")]
+        public ActionResult<IEnumerable<Good>> RetrieveGoods()
+        {
+            return new Good[] 
+            {
+                new Good() { Name = "Soccer ball",  Description = "New soccer ball", Price = 111.0m },
+                new Good() { Name = "Foot ball",  Description = "New foot ball", Price = 111.0m },
+            };
+        }
+
+        [HttpGet]
+        [Route("retrievePets")]
+        public ActionResult<IEnumerable<Pets>> RetrievePets()
+        {
+            return new Pets[]
+            {
+                new Pets() { Name = "Cat",  Description = "Cute cat", Price = 111.0m },
+                new Pets() { Name = "Dog",  Description = "Good boy", Price = 111.0m },
+            };
+        }
+    }
+}
---
+++ MyWebApi/Models/Good.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApi.Models
+{
+    public class Good
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
